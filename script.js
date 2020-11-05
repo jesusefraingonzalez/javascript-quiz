52ec0b47a8d102e59efe4c65ac0fc697a481d782
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2020-11-05 03:20:07 -0500

    add array of question objects

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-var 
+var questions = [
+    {
+        question: "Commonly used data types do not include:",
+        answers: ["strings", "booleans", "alerts", "numbers"],
+        correct: "alerts"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within:",
+        answers: ["quotes", "parentheses", "curly brackets", "square brackets"],
+        correct: "parentheses"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store: ",
+        answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        correct: "all of the above"
+    },
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables",
+        answers: ["commas", "curly brackets", "quotes", "parentheses"],
+        correct: "quotes"
+    },
+    {
+        question: "",
+        answers: ["", "", "", ""],
+        correct: ""
+    },
+    // {
+    //     question: "",
+    //     answers: ["", "", "", ""],
+    //     correct: ""
+    // }
+]
